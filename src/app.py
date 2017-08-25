14f49f8c6445f3bc30d9c622197d600b3c93d53c
Max Bo <max@maxbo.me>
2017-08-25 18:25:11 +1000

    Stub out the project using NLP example

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+def syntax_text(text):
+    client = language.LanguageServiceClient()
+
+    # Instantiates a plain text document.
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects syntax in the document. You can also analyze HTML with:
+    #   document.type == enums.Document.Type.HTML
+    tokens = client.analyze_syntax(document).tokens
+
+    # part-of-speech tags from enums.PartOfSpeech.Tag
+    pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM',
+               'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
+
+    for token in tokens:
+        print(u'{}: {}'.format(pos_tag[token.part_of_speech.tag],
+                               token.text.content))
+
+syntax_text("Hello world!")
