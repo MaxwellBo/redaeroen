af2ab683b9b408e0718137796b4014d90919a290
Max Bo <max@maxbo.me>
2017-08-25 20:33:46 +1000

    Implement dependency edge descriptions

--- src/app.py
+++ src/app.py
@@ -4,6 +4,9 @@ from google.cloud.language import types
 from google.cloud.language.syntax import PartOfSpeech
 from beautifultable import BeautifulTable
 
+POS_LOOKUP = {v: k for k, v in enums.PartOfSpeech.Tag.__dict__.items()}
+LABEL_LOOKUP = {v: k for k, v in enums.DependencyEdge.Label.__dict__.items()}
+
 def process(text):
     client = language.LanguageServiceClient()
 
@@ -15,19 +18,46 @@ def process(text):
 
 def render_tokens(tokens):
 
-    def align(content, part_of_speech):
-        return [ content, part_of_speech ]
+    def align( content=""
+             , begin=""
+             , part_of_speech=""
+             , parent_content=""
+             , edge_index=""
+             , edge_label=""
+             , lemma=""
+             ):
+        return [ content
+            #    , begin
+               , part_of_speech
+               , parent_content
+            #    , edge_index
+               , edge_label
+               , lemma 
+               ]
 
     def parse(token):
-        pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM',
-                'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
+        verbose_tag = PartOfSpeech.reverse(POS_LOOKUP[ token.part_of_speech.tag ])
 
-        verbose_tag = PartOfSpeech.reverse(pos_tag[ token.part_of_speech.tag ])
-
-        return align(content=i.text.content, part_of_speech=verbose_tag)
+        parent = tokens[token.dependency_edge.head_token_index].text
+        
+        return align( content=token.text.content
+                    , begin=token.text.begin_offset
+                    , part_of_speech=verbose_tag
+                    , edge_index=token.dependency_edge.head_token_index
+                    , parent_content=parent.content
+                    , edge_label=LABEL_LOOKUP[token.dependency_edge.label]
+                    , lemma=token.lemma
+                    , )
 
     table = BeautifulTable()
-    table.column_headers = align(content="content", part_of_speech="part of speech")
+    table.column_headers = align( content="content" 
+                                , begin="begin offset"
+                                , part_of_speech="part of speech"
+                                , parent_content="parent content"
+                                , edge_index="edge index"
+                                , edge_label="edge label"
+                                , lemma="lemma"
+                                )
 
     for i in tokens: table.append_row(parse(i))
     print(table)
