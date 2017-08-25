f38ebfcd36f7e024b9ae120cdb608e004553f754
Max Bo <max@maxbo.me>
2017-08-25 21:15:58 +1000

    Do parent-child analysis

--- src/nlp.py
+++ src/nlp.py
@@ -5,6 +5,8 @@ from google.cloud.language.syntax import PartOfSpeech
 from beautifultable import BeautifulTable
 
 POS_LOOKUP = {v: k for k, v in enums.PartOfSpeech.Tag.__dict__.items()}
+
+# https://github.com/GoogleCloudPlatform/google-cloud-python/blob/master/language/google/cloud/gapic/language/v1/enums.py
 LABEL_LOOKUP = {v: k for k, v in enums.DependencyEdge.Label.__dict__.items()}
 
 def process(text):
@@ -17,11 +19,17 @@ def process(text):
     return client.analyze_syntax(document).tokens
 
 def render_tokens(tokens):
+    def get_children(token):
+        return [ i for i in tokens if get_parent(i) is token ] 
+
+    def get_parent(token):
+        return tokens[token.dependency_edge.head_token_index]
 
     def align( content=""
              , begin=""
              , part_of_speech=""
              , parent_content=""
+             , children_content=""
              , edge_index=""
              , edge_label=""
              , lemma=""
@@ -30,6 +38,7 @@ def render_tokens(tokens):
             #    , begin
                , part_of_speech
                , parent_content
+               , children_content
             #    , edge_index
                , edge_label
                , lemma 
@@ -38,22 +47,21 @@ def render_tokens(tokens):
     def parse(token):
         verbose_tag = PartOfSpeech.reverse(POS_LOOKUP[ token.part_of_speech.tag ])
 
-        parent = tokens[token.dependency_edge.head_token_index].text
-        
         return align( content=token.text.content
                     , begin=token.text.begin_offset
                     , part_of_speech=verbose_tag
-                    , edge_index=token.dependency_edge.head_token_index
-                    , parent_content=parent.content
+                    , parent_content=get_parent(token).text.content
+                    , children_content=str([i.text.content for i in get_children(token) ])
                     , edge_label=LABEL_LOOKUP[token.dependency_edge.label]
                     , lemma=token.lemma
                     , )
 
-    table = BeautifulTable()
+    table = BeautifulTable(max_width=120)
     table.column_headers = align( content="content" 
                                 , begin="begin offset"
                                 , part_of_speech="part of speech"
                                 , parent_content="parent content"
+                                , children_content="children content"
                                 , edge_index="edge index"
                                 , edge_label="edge label"
                                 , lemma="lemma"
