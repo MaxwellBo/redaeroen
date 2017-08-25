47bb6e4f045b720454c4a7557780634961b62df3
Max Bo <max@maxbo.me>
2017-08-25 18:44:19 +1000

    Tableify the AST

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 from google.cloud.language.syntax import PartOfSpeech
+from beautifultable import BeautifulTable
 
 def process(text):
     client = language.LanguageServiceClient()
@@ -13,12 +14,22 @@ def process(text):
     return client.analyze_syntax(document).tokens
 
 def render_tokens(tokens):
-    # part-of-speech tags from enums.PartOfSpeech.Tag
-    pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM',
-               'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
 
-    for token in tokens:
-        print(u'{}: {}'.format(pos_tag[token.part_of_speech.tag],
-                               token.text.content))
+    def align(content, part_of_speech):
+        return [ content, part_of_speech ]
 
-render_tokens(process("Hello world!"))
+    def parse(token):
+        pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM',
+                'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
+
+        verbose_tag = PartOfSpeech.reverse(pos_tag[ token.part_of_speech.tag ])
+
+        return align(content=i.text.content, part_of_speech=verbose_tag)
+
+    table = BeautifulTable()
+    table.column_headers = align(content="content", part_of_speech="part of speech")
+
+    for i in tokens: table.append_row(parse(i))
+    print(table)
+
+render_tokens(process("delete the first two words from the next line"))
