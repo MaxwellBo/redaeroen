11fd72cc6b3b971a408f7051e45dea03827db107
Joseph <joe.spearritt@live.com>
2017-08-25 21:09:46 +1000

    Voice rec doesn't quite work but is cool

--- voicerec/voicerec.py
+++ voicerec/voicerec.py
@@ -1,8 +1,10 @@
 import sys
 import pyaudio
+import re
 
 from six.moves import queue
 
+import grpc
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
@@ -16,7 +18,7 @@ class MicGen(object):
 
     def __enter__(self):
         self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(channels=1, 
+        self._audio_stream = self._audio_interface.open(format=pyaudio.paInt16, channels=1, 
                                                         rate=self._rate, input=True, 
                                                         frames_per_buffer=self._chunk_size, 
                                                         stream_callback=self._fill_buffer)
@@ -54,31 +56,69 @@ class MicGen(object):
                     data.append(chunk)
                 except queue.Empty:
                     break
-
-            yield ''.join(data)
+            # print("<Yo>")
+            yield b''.join(data)
 
 
 class VoiceRecogniser(object):
 
-    def __init__(self, input_stream):
+    def __init__(self, input_stream, command_phrase=None):
         self._input_stream = input_stream
         self._client = speech.SpeechClient()
         self._speech_config = types.RecognitionConfig(
-                                encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-                                sample_rate_hertz=input_stream.get_rate(),
-                                language_code='en-AU')
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=input_stream.get_rate(),
+            language_code='en-US')
         self._streaming_config = types.StreamingRecognitionConfig(
-                                config=self._speech_config,
-                                interim_results=False
+            config=self._speech_config,
+            single_utterance=True,
+            interim_results=False
         )
-
-    def generator(self):
-        requests = (types.StreamingRecognitionRequest(audio_content=content) 
-                    for content 
-                    in self._input_stream.generator())
+        self._command_phrase=re.compile(command_phrase)
+        gen = self._input_stream.generator()
+        self._requests = (types.StreamingRecognizeRequest(audio_content=content)
+                    for content
+                    in gen)
         
-        responses = self._client.streaming_recognize(self._streaming_config, requests)
 
-        for response in responses:
-            pass
+    def _process_response(self, response):
+        if not response.results:
+            return None
 
+        # Check for multiple results
+        result = response.results[0]
+        if not result.alternatives[0]:
+            return None
+
+        # Get the top alternative
+        transcript = result.alternatives[0].transcript
+
+        # print("<" + str(result.is_final) + ">: " + transcript) 
+        # Only use final results
+        # print("choosing:")
+        if result.is_final:
+            print("<command>") 
+            return transcript
+        else:
+            # print("<inprog>: " + transcript) 
+            return None
+
+        # Check for the command phrase
+        # if not re.search(self._command_phrase, transcript, re.I):
+        #     continue
+
+    def generator(self): 
+        while True:
+            response = self._client.streaming_recognize(self._streaming_config, 
+                                                        self._requests)
+            transcript = self._process_response(next(response))
+
+            if transcript is not None:
+                yield transcript
+
+if __name__ == '__main__':
+    with MicGen(16000, int(16000/10)) as stream:
+        vr = VoiceRecogniser(stream, 'stupid')
+        # Print things out
+        for command in vr.generator():
+            print("COMMAND: " + command)
