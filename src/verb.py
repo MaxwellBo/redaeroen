e8b42abc26b81fec603a3812c61244cafbc9f3b2
Max Bo <max@maxbo.me>
2017-08-26 20:21:41 +1000

    Final

--- src/verb.py
+++ src/verb.py
@@ -1,23 +1,7 @@
-"""
-|c|	c	change
-|d|	d	delete
-|y|	y	yank into register (does not change the text)
-|~|	~	swap case (only if 'tildeop' is set)
-|g~|	g~	swap case
-|gu|	gu	make lowercase
-|gU|	gU	make uppercase
-|!|	!	filter through an external program
-|=|	=	filter through 'equalprg' or C-indenting if empty
-|gq|	gq	text formatting
-|g?|	g?	ROT13 encoding
-|>|	>	shift right
-|<|	<	shift left
-|zf|	zf	define a fold
-|g@|	g@	call function set with the 'operatorfunc' option
-"""
-
 class Verb(object):
-    pass
+    LOOKUP = {"change", Change
+             ,"delete", Delete
+             }
 
 class Change(Verb):
     SELF = "Y"
